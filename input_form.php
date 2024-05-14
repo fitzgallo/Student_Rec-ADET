0c6ffa4610b68bfff1e32bc1da20fc2b28d1ad48
fitzgallo <160306902+fitzgallo@users.noreply.github.com>
2024-05-14 18:23:04 +0800

    Update input_form.php

--- input_form.php
+++ input_form.php
@@ -13,6 +13,5 @@ Age: <br><input type="text" name="age"><br>
 Grade: <br><input type="text" name="grade"><br>
 <input type="submit">
 </form>
-<p><a href="home.php">Back to Home</a></p>
 </body>
 </html>
